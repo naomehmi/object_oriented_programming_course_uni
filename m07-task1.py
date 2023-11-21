8172aa9ef175e7a223d62a8d919bb32d9e3012d6
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-11-21 09:49:01 +0700

    Update m07-task1.py

--- m07-task1.py
+++ m07-task1.py
@@ -2,21 +2,24 @@ from class_k import NaomiPrisellaM7_1
 
 data = NaomiPrisellaM7_1()
 
+pil1 = None
+
 while True:
     try:
         print("Menu Utama")
         print("==========\n")
         print("1. Input absensi mahasiswa baru")
-        print("2. Cari mahasiswa")
-        print("3. Tampil data mahasiswa")
+        print("2. Tampil data mahasiswa")
+        print("3. Cari mahasiswa")
         print("4. Keluar\n")
 
-        pil1 = int(input("Pil1ih salah satu (1/2/3) : "))
+        pil1 = int(input("Pilih salah satu (1/2/3) : "))
 
         if pil1 < 1 or pil1 > 4:
             raise ValueError
-        
+
         print()
+
     except ValueError:
         print("\nInput angka 1, 2, atau 3.\n")
 
@@ -29,37 +32,39 @@ while True:
             x = input("NIM Mahasiswa : ").lstrip().rstrip()
             y = input("Nama Mahasiswa : ").title().lstrip().rstrip()
             z = input("Nomor HP Mahasiswa : ").lstrip().rstrip()
+            print()
             if not data.validasi(x,y,z):
                 continue
+            print("Berhasil diinput")
             a.send(x)
             a.send(y)
             a.send(z)
-            print("\nJumlah mahasiswa yang telah absensi :",len(data.mahasiswa),"\n")
+            print("\nJumlah mahasiswa yang telah absensi : ", len(data.mahasiswa),"\n")
 
             while True:
                 try:
-                    pil2 = input("Apakah masih mau input mahasiswa baru? (Y/N) : ").lower()
-                    
+                    pil2 = input("Apakah masih mau input mahasiswa? (Y/N) : ").lower()
+
                     if pil2 != 'y' and pil2 != 'n':
                         raise ValueError("Jawaban hanya boleh berupa 'y' atau 'n'")
-                    
+
                     break
-                    
+
                 except ValueError as e:
                     print(str(e))
 
     elif pil1 == 2:
-        cari = input("NIM mahasiswa yang ingin dicari: ")
-        data.pencarianData(cari)
-
-    elif pil1 == 3:
         if not data.mahasiswa:
-            print("Data masih kosong. Input dulu.")
+            print("Data masih kosong. Input dulu")
         else:
-            print("Berikut data yang telah diurutkan:\n")
+            print("Berikut data yang telah diurutkan : \n")
             data.cetakData()
 
-    else:
+    elif pil1 == 3:
+        cari = input("NIM Mahasiswa yang ingin dicari: ")
+        data.pencarianData(cari)
+
+    elif pil1 == 4:
         break
 
     print()
